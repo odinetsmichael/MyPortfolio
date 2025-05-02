1668e0359d10a350471ef1a95c65714c7018795d
Mykhailo <odynetsmykhailo@gmail.com>
2025-05-02 17:41:02 +0300

    added new job exp

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,18 @@
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  closeElem = document.querySelector(".menu__close");
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
 });
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+closeElem.addEventListener("click", () => {
+  menu.classList.remove("active");
 });
 
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+const counters = document.querySelectorAll(".skills__ratings-counter"),
+  lines = document.querySelectorAll(".skills__ratings-line span");
 
-counters.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
+counters.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
 });
-
